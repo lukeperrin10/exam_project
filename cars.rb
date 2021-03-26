a6817e092b41cb4b4eab09f250b0e908f161edc1
lukeperrin10 <lukeperrin27@gmail.com>
2021-03-26 13:33:11 +0100

    added first answers to ruby questions

---
+++ cars.rb
@@ -0,0 +1,13 @@
+
+
+new = 'variable' #this is a string data type.
+
+#other 5 data types: 
+
+    #number(floating point, interger)
+    #Boolean (true or false)
+    #arrays(store data)
+    #hashes(assign values to a key)
+    #symbols(are used instead of strings to save on memory)
+
+    
