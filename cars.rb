eb6af0b7195eba9e83422341ea83154ada61f008
lukeperrin10 <lukeperrin27@gmail.com>
2021-03-26 14:13:42 +0100

    added second part to ruby questions

--- cars.rb
+++ cars.rb
@@ -9,5 +9,15 @@ new = 'variable' #this is a string data type.
     #arrays(store data)
     #hashes(assign values to a key)
     #symbols(are used instead of strings to save on memory)
+##########
 
-    
+
+
+cars = Array.new
+
+    cars.push("Passat")
+    cars.push("Golf")
+
+
+cars[1] = {wheels:'19 inch',max_speed:'240kph'color:'white'}
+
